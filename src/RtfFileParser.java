2f0282e5f8b12750a980e241c45253fee7c4c9b7
Vadim Matsishin <matsishin.vadim@gmail.com>
2016-12-20 21:27:43 +0300

    added file property and constructor to RtfFileParser

--- src/RtfFileParser.java
+++ src/RtfFileParser.java
@@ -1,12 +1,21 @@
+import java.io.File;
 import java.util.List;
 
 /**
  * Created by vadim on 20/12/16.
  */
 public class RtfFileParser implements ScheduleParser {
+
+    private File file;
+
+    public RtfFileParser(File file) {
+        this.file = file;
+    }
+
+
     @Override
     public List<Record> parse() {
-        //// TODO: 20/12/16  
+        //// TODO: 20/12/16
         return null;
     }
 }
