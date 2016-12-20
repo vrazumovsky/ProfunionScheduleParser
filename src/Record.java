069fd18d31b66eaaf91bf9764c59112384ad0001
Vadim Matsishin <matsishin.vadim@gmail.com>
2016-12-21 00:11:02 +0300

    record low

--- src/Record.java
+++ src/Record.java
@@ -1,22 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * Created by vadim on 20/12/16.
  */
-public abstract class Record {
-    private String institute;
-    private String startTime;
-    private String weekDay;
-    private String name;
-    private String group;
-    private String type;
-    private List<String> teachers = new ArrayList<>();
-    private List<String> rawData;
+public class Record {
+    protected String institute;
+    protected String startTime;
+    protected String weekDay;
+    protected String name;
+    protected String group;
+    protected String type;
+    protected String tabOffset;
+    protected List<String> teachers = new ArrayList<>();
 
-    public Record(List<String> rawData) {
-        this.rawData = rawData;
+
+    @Override
+    public String toString() {
+        return "Record{" +
+                "institute='" + institute + '\'' +
+                ", startTime='" + startTime + '\'' +
+                ", weekDay='" + weekDay + '\'' +
+                ", name='" + name + '\'' +
+                ", group='" + group + '\'' +
+                ", teachers=" + teachers +
+                '}';
     }
-
-
 }
