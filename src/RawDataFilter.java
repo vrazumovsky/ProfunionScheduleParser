535da16d7da1e80798e56addd929f5c324981edf
Vadim Matsishin <matsishin.vadim@gmail.com>
2016-12-20 22:18:33 +0300

    added raw data filter by institutes

---
+++ src/RawDataFilter.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by vadim on 20/12/16.
+ */
+public class RawDataFilter {
+    private static final List<String> institutes;
+
+    static {
+        String[] institutesArray = new String[] {"Институт прокуратуры РФ", "Политология", "Институт юстиции",
+                "Правоохранительная деятельность", "Институт правоохранительной деятельности",
+                "Правовое обеспечение национальной безопасности", "безопасности"};
+        institutes = Arrays.asList(institutesArray);
+    }
+
+    private List<TableString> rawData;
+
+    public RawDataFilter(List<TableString> rawData) {
+        this.rawData = rawData;
+    }
+
+    public List<TableString> filter() {
+        for (int i = 0; i < rawData.size(); i++) {
+            if (rawData.get(i).matchesOneOf(institutes)) {
+                return rawData.subList(i, rawData.size());
+            }
+        }
+        return rawData;
+    }
+}
