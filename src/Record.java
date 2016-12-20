8ebb774eb09f1a8fb21cedcd267f5ff923577f60
Vadim Matsishin <matsishin.vadim@gmail.com>
2016-12-20 21:21:29 +0300

    Added fields in Record class

--- src/Record.java
+++ src/Record.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by vadim on 20/12/16.
  */
 public abstract class Record {
+    private String institute;
+    private String startTime;
+    private String weekDay;
+    private String name;
+    private String group;
+    private String type;
+    private List<String> teachers = new ArrayList<>();
+    private List<String> rawData;
+
+    public Record(List<String> rawData) {
+        this.rawData = rawData;
+    }
 }
