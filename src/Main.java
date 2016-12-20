dcac5c5f304ae3286e67bdcd2420dc61890afffe
Vadim Matsishin <matsishin.vadim@gmail.com>
2016-12-20 21:40:51 +0300

    added FILES_DIRECTORY static field added simple Main implementation

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.util.List;
+
 /**
  * Created by vadim on 20/12/16.
  */
 public class Main {
-    public static void main(String[] args) {
 
+    private static final String FILES_DIRECTORY = "/Users/vadim/Desktop/exams";
+
+    public static void main(String[] args) {
+        ScheduleParser parser = new FilesParser(FILES_DIRECTORY);
+
+        List<Record> allRecords = parser.parse();
+
+        System.out.println(allRecords);
     }
 }
