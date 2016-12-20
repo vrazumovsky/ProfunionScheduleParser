d8476f3b8e64ace239d9addfda79cbcca019d36c
Vadim Matsishin <matsishin.vadim@gmail.com>
2016-12-20 21:48:59 +0300

    RtfListener moved to RtfParser, added parse implementation

--- src/RtfFileParser.java
+++ src/RtfFileParser.java
@@ -1,4 +1,14 @@
+import com.rtfparserkit.parser.IRtfListener;
+import com.rtfparserkit.parser.IRtfParser;
+import com.rtfparserkit.parser.IRtfSource;
+import com.rtfparserkit.parser.RtfStreamSource;
+import com.rtfparserkit.parser.standard.StandardRtfParser;
+import com.rtfparserkit.rtf.Command;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +27,54 @@ public class RtfFileParser implements ScheduleParser {
 
     @Override
     public List<Record> parse() {
-        //// TODO: 20/12/16
+        InputStream is = null;
+        try {
+            is = new FileInputStream(file);
+
+            IRtfSource source = new RtfStreamSource(is);
+            IRtfParser parser = new StandardRtfParser();
+            IRtfListener listener = new RtfListener();
+            parser.parse(source, listener);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return records;
+    }
+
+    private class RtfListener implements IRtfListener {
+
+        @Override
+        public void processDocumentStart() {
+            System.out.println("processDocumentStart");
+        }
+
+        @Override
+        public void processDocumentEnd() {
+            System.out.println("processDocumentEnd");
+        }
+
+        @Override
+        public void processGroupStart() {}
+
+        @Override
+        public void processGroupEnd() {}
+
+        @Override
+        public void processCharacterBytes(byte[] bytes) {}
+
+        @Override
+        public void processBinaryBytes(byte[] bytes) {}
+
+        @Override
+        public void processString(String s) {
+
+        }
+
+        @Override
+        public void processCommand(Command command, int i, boolean b, boolean b1) {
+
+        }
+
     }
 }
