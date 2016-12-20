33935c1546e4dea383054a5a405427682ab572e8
Vadim Matsishin <matsishin.vadim@gmail.com>
2016-12-20 21:24:10 +0300

    added RtfFileParser interface

---
+++ src/RtfFileParser.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+/**
+ * Created by vadim on 20/12/16.
+ */
+public class RtfFileParser implements ScheduleParser {
+    @Override
+    public List<Record> parse() {
+        //// TODO: 20/12/16  
+        return null;
+    }
+}
