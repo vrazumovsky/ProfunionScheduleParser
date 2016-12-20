2186546c0e4b50c652ca55f860e1c031db1f7526
Vadim Matsishin <matsishin.vadim@gmail.com>
2016-12-21 00:27:48 +0300

    added Teacher class

---
+++ src/Teacher.java
@@ -0,0 +1,21 @@
+/**
+ * Created by vadim on 21/12/16.
+ */
+public class Teacher {
+
+    private String name;
+    private String cabinet;
+
+    public Teacher(String name, String cabinet) {
+        this.name = name;
+        this.cabinet = cabinet;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCabinet() {
+        return cabinet;
+    }
+}
