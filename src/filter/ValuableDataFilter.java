dd2e4b2de640898513fd82f423229e6fef0f4656
Vadim Matsishin <matsishin.vadim@gmail.com>
2017-01-27 13:25:12 +0300

    added several crutches because of mistakes in data files to valuable data filter

--- src/filter/ValuableDataFilter.java
+++ src/filter/ValuableDataFilter.java
@@ -3,6 +3,7 @@ package filter;
 import data.raw.TableString;
 import filter.Filter;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,7 +29,23 @@ public class ValuableDataFilter implements Filter {
                 if (tableString.getString().equals("безопасности") || tableString.getString().equals(" безопасности")) {
                     tableString.setString("Правовое обеспечение национальной безопасности");
                 }
-                return rawData.subList(i, rawData.size());
+
+                List<TableString> valuableData = new ArrayList<>();
+                for (TableString string : rawData.subList(i, rawData.size())) {
+                    if (!string.getString().equals(" ")) {
+                        int lastOffset = 0;
+                        if (valuableData.size() > 0) {
+                            lastOffset = valuableData.get(valuableData.size() - 1).getTabOffset();
+                        }
+                        if (string.getTabOffset() == lastOffset) {
+                            TableString last = valuableData.remove(valuableData.size() - 1);
+                            valuableData.add(new TableString(last.getString() + string.getString(), lastOffset));
+                        } else {
+                            valuableData.add(string);
+                        }
+                    }
+                }
+                return valuableData;
             }
         }
         return rawData;
