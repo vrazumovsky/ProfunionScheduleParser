86fc5ce9914a80e470fc21f32a22ecfab0ff8238
Vadim Matsishin <matsishin.vadim@gmail.com>
2017-01-27 10:18:27 +0300

    ExamRecords refactor createRecords method

--- src/data/record/ExamRecords.java
+++ src/data/record/ExamRecords.java
@@ -15,19 +15,57 @@ public class ExamRecords {
     private static final Pattern DATE_PATTERN = Pattern.compile("[0-9]+[-][а-яА-Я]+[-][0-9]+");
     private List<TableString> rawData;
 
+    private List<Record> records = new ArrayList<>();
+    private String institute;
+    private List<String> groups = new ArrayList<>();
+    private int recordsStartIndex = 0;
+    private List<Integer> offsets = new ArrayList<>();
+
+
     public ExamRecords(List<TableString> rawData) {
         this.rawData = rawData;
     }
 
     public List<Record> createRecords() {
-        List<Record> records = new ArrayList<>();
+        extractInstitute();
+        extractGroups();
+        extractExamRecords();
+        extractSortedTabOffsets();
+        setGroupToRecords();
 
-        String institute = rawData.get(0).getString();
-        List<String> groups = new ArrayList<>();
+        return records;
+    }
 
+    private void setGroupToRecords() {
+        for (Record record : records) {
+            int groupIndex = offsets.indexOf(record.tabOffset);
+            record.group = groups.get(groupIndex);
+        }
+    }
 
-        int recordsStartIndex = 0;
+    private void extractSortedTabOffsets() {
+        for (Record record : records) {
+            if (!offsets.contains(record.tabOffset)) {
+                offsets.add(record.tabOffset);
+            }
+        }
 
+        Collections.sort(offsets);
+    }
+
+    private void extractExamRecords() {
+        int firstIndex = recordsStartIndex;
+        for (int i = recordsStartIndex + 1; i < rawData.size(); i++) {
+            if (stringWithIndexMatches(i) || i == rawData.size() - 1) {
+                SameStartTimeRecords sameStartTimeRecords = new SameStartTimeRecords(
+                        institute, rawData.subList(firstIndex, i == rawData.size() - 1 ? rawData.size() : i));
+                records.addAll(sameStartTimeRecords.createRecords());
+                firstIndex = i;
+            }
+        }
+    }
+
+    private void extractGroups() {
         for (int i = 1; i < rawData.size(); i++) {
             if (stringWithIndexMatches(i)) {
                 recordsStartIndex = i;
@@ -36,35 +74,12 @@ public class ExamRecords {
                 groups.add(rawData.get(i).getString());
             }
         }
-
-        int firstIndex = recordsStartIndex;
-        for (int i = recordsStartIndex + 1; i < rawData.size(); i++) {
-            if (stringWithIndexMatches(i) || i == rawData.size() - 1) {
-                SameStartTimeRecords sameStartTimeRecords =
-                        new SameStartTimeRecords(institute, rawData.subList(firstIndex, i == rawData.size() - 1 ? rawData.size() : i));
-                records.addAll(sameStartTimeRecords.createRecords());
-                firstIndex = i;
-            }
-        }
-
-        List<Integer> offsets = new ArrayList<>();
-
-        for (Record record : records) {
-            if (!offsets.contains(record.tabOffset)) {
-                offsets.add(record.tabOffset);
-            }
-        }
-
-        Collections.sort(offsets);
-
-        for (Record record : records) {
-            int groupIndex = offsets.indexOf(record.tabOffset);
-            record.group = groups.get(groupIndex);
-        }
-
-
-        return records;
     }
+
+    private void extractInstitute() {
+        institute = rawData.get(0).getString();
+    }
+
 
     private boolean stringWithIndexMatches(int i) {
         return DATE_PATTERN.matcher(rawData.get(i).getString()).matches();
